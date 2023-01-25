d36ba38f14149e701cf6ec85edc2605d199b2e4e
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-01-24 23:53:31 -0600

    Mostrar los resultados de consulta

--- js/app.js
+++ js/app.js
@@ -43,7 +43,7 @@ function mostrarAlerta(mensaje){
 
 function buscarImagenes(termino) {
     const key = `33101128-ff9f4cadad411689fc1242ff6`;
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
 
     fetch(url)
         .then(respuesta => respuesta.json())
@@ -53,5 +53,27 @@ function buscarImagenes(termino) {
 }
 
 function mostrarImagenes(imagenes){
-    console.log(imagenes);
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+        }
+        //Iterar sobre el arreglo de imagenes y construir el HTML
+        imagenes.forEach(imagen => {
+            const {previewURL, likes, views, largeImageURL} = imagen;
+
+            resultado.innerHTML += `
+                <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                    <div class="bg-white">
+                        <img class="w-full" src="${previewURL}">
+                        <div class="p-4">
+                            <p class="font-bold">${likes} <span class="font-light">Me Gusta</span></p>
+                            <p class="font-bold">${views} <span class="font-light">Vistas</span></p>
+                            <a class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"href="${largeImageURL}" target="blank" rel="noopener noreferrer">Ver Imagen</a>
+                        </div>
+                    </div>
+                </div>
+            `;
+
+        });
+
+    
 }
